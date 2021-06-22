b4b28d23da07f6bad7189d5922209cb808117481
rogelio-lopez <rogejlopez@gmail.com>
2021-06-22 16:41:30 -0400

    added sidebars + effects

--- src/App.js
+++ src/App.js
@@ -1,29 +1,83 @@
 import React, { useState } from 'react';
 import './App.css';
-import Burger from './menu/Burger';
-import Menu from './menu/Menu';
 
 
 function App() {
 
-  const [openMenu, setOpenMenu] = useState(false);
+  const [menuClick, setmenuClick] = useState(false);
+
+  const toggleMenu = () => setmenuClick(!menuClick); 
 
   return (
     <div className="app">
 
-      <div className="app__menu">
-        <Burger openMenu={openMenu} setOpenMenu={setOpenMenu}/>
-        <Menu openMenu={openMenu} />
+      <div className="rightSide">
+        <div className="email">
+          <a>rogejlopez@gmail.com</a>
+        </div>
       </div>
 
-      <div className="app__sections">
-        {/* Header */}
+      <div className={(menuClick)? 'leftSide menu-active' : 'leftSide'}>
+        
+        <i className="menu-burger fas fa-bars" onClick={toggleMenu}></i>
 
-        {/* About */}
+        {/* nav items */}
+        <ul className="nav-buttons">
+          <li><a>About</a></li>
+          <li><a>Projects</a></li>
+          <li><a>Contact</a></li>
+        </ul>
 
-        {/* Skills */}
+        {/* resume */}
+        <div className="resume">
+          <button>resume</button>
+        </div>
 
-        {/* Projects */}
+        <div className="socials-container">
+          <h4>Socials</h4>
+          <ul className="socials">
+            <li><a><i className="fab fa-github"></i></a></li>
+            <li><a><i className="fab fa-linkedin-in"></i></a></li>
+            <li><a><i className="fab fa-codepen"></i></a></li>
+            <li><a><i className="fab fa-instagram"></i></a></li>
+          </ul>
+        </div>
+      </div>
+
+
+
+
+      {/* Notes
+
+        - Nav bar 
+        - with resume button
+        - No logo
+      
+        https://brittanychiang.com/ */}
+
+
+      <div className="app__main">
+        {/* 
+          Header 
+          - Hello message
+          - Quick introduction
+            - what i do 
+            - what i like 
+        */}
+
+        {/* 
+          About 
+          - about myself (paragraph- short and to the point)
+          - Skills (add the ones most used- frontend)
+        */}
+
+        {/* 
+          Projects 
+          - mixture of  
+            - https://brittanychiang.com/
+            - http://riccardozanutta.com/
+        */}
+
 
         {/* Contact */}
       </div>
--- src/data/UserData.js
+++ src/data/UserData.js
@@ -1,6 +1,6 @@
 
-// HERO DATA
-export const heroData = {
+// MY DATA
+export const myData = {
     name: 'Rogelio',
     fullname: 'Rogelio Lopez',
     title: 'Web Developer',
@@ -8,9 +8,8 @@ export const heroData = {
 
 // ABOUT ME DATA
 export const aboutData = {
-    img: 'img-canyon.jpg',
-    aboutMe: 'Phasellus suscipit placerat elit nec cursus. Nulla aliquet venenatis tellus et pretium. Nullam pretium nunc lorem, eget consequat justo suscipit eu. Nam non sapien libero. Pellentesque ut dolor mollis, commodo erat id, sollicitudin ante. Maecenas nec rhoncus enim. Nunc dignissim vitae ex id mollis.',
-    resume: ''
+    img: '',
+    aboutMe: 'Phasellus suscipit placerat elit nec cursus. Nulla aliquet venenatis tellus et pretium. Nullam pretium nunc lorem, eget consequat justo suscipit eu. Nam non sapien libero. Pellentesque ut dolor mollis, commodo erat id, sollicitudin ante. Maecenas nec rhoncus enim. Nunc dignissim vitae ex id mollis.'
 }
 
 // SKILLS DATA
