71b60d266c8c1348fdfb753d36d1f169c2309d5b
rogelio-lopez <rogejlopez@gmail.com>
2021-08-18 17:06:38 -0400

    adding projects

--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -6,16 +6,22 @@ function Projects() {
 
   const projects = [
     {
-      name: 'IWPS Services',
-      description: 'IWPS Services is an international wealth planning company.',
-      tech: ['html', 'css', 'react'],
-      link: 'www.google.com'
-    },
-    {
       name: 'Kim\'s Kuban',
       description: 'Kim\'s Kuban is a popular Key West restaurant.',
       tech: ['html', 'css', 'react'],
       link: 'https://kimskuban.com/'
+    },
+    {
+      name: 'IWPS Services',
+      description: 'IWPS Services is an international wealth planning company.',
+      tech: ['html', 'css', 'react'],
+      link: 'https://iwps-a87bf.web.app/'
+    },
+    {
+      name: 'Amazon Clone',
+      description: 'Amazon clone using React and Firebase authentication.',
+      tech: ['react'],
+      link: 'https://clone-92a6d.web.app/'
     },
   ]
 
